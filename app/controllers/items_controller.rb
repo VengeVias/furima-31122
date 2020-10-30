1bfcfadec53eefc8df4d60edd4708d9c87a32747
VengeVias <rocktbh@gmail.com>
2020-10-30 17:48:26 +0900

    コントローラ、バリデーション、ルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,23 @@
 class ItemsController < ApplicationController
   def index
-    @user = User.all
+    @item = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, :price, :user)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :condition, :delivery_charge, :prefecture, :days_to_ship
+
+  validates :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, :price, :user, presence: true
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, numericality: { other_than: 1 }
 end
